5fce94e41ccd594de343749f1d49b9ff50d1d48e
Oliver Lando <oliverlandootieno@gmail.com>
2020-04-30 10:04:53 +0300

    Update index.php

--- login/index.php
+++ login/index.php
@@ -1,1 +1,46 @@
 
+<?php
+// core configuration
+include_once "config/core.php";
+ 
+// set page title
+$page_title="Checkout";
+ 
+// include login checker
+$require_login=true;
+include_once "login_checker.php";
+ 
+// include page header HTML
+include_once 'layout_head.php';
+ 
+echo "<div class='col-md-12'>";
+ 
+    // to prevent undefined index notice
+    $action = isset($_GET['action']) ? $_GET['action'] : "";
+ 
+    // if login was successful
+    if($action=='login_success'){
+        echo "<div class='alert alert-info'>";
+            echo "<strong>Hi " . $_SESSION['firstname'] . ", Proceed with your checkout !</strong>";
+        echo "</div>";
+    }
+ 
+    // if user is already logged in, shown when user tries to access the login page
+    else if($action=='already_logged_in'){
+        echo "<div class='alert alert-info'>";
+            echo "<strong>You are already logged in.</strong>";
+        echo "</div>";
+    }
+ 
+    // content once logged in
+    echo "<div class='alert alert-info'>";
+        echo "<iframe src='../checkout.php' height='900' width='850'></iframe>";
+    echo "</div>";
+ 
+echo "</div>";
+ 
+// footer HTML and JavaScript codes
+include 'layout_foot.php';
+?>
+
+
